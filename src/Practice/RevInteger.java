f0213e4d1b9f9ce6b89b29dff68707d47303eb15
sbhr574 <sbhr574@gmail.com>
2023-04-19 11:42:26 +0530

    reverse integer without loop 19-04-23

--- src/Practice/RevInteger.java
+++ src/Practice/RevInteger.java
@@ -2,6 +2,18 @@ package Practice;
 
 public class RevInteger
 {
+    //without loop
+    public static void reverseMethod(int number)
+    {
+        if (number < 10)
+        {
+            System.out.println(number);
+        } else
+        {
+            System.out.print(number % 10);
+            reverseMethod(number / 10);
+        }
+    }
     public static void main(String[] args) {
         int num = 1234;
         int rev = 0;
@@ -10,6 +22,8 @@ public class RevInteger
             rev = rev*10 + num%10;
             num = num/10;
         }
-//        System.out.println(rev);
+        System.out.println(rev);
+
+        reverseMethod(56789);
     }
 }
