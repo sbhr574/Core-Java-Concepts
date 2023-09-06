dae47acdc8a59879b4dc3be407081593ab0cc82e
sbhr574 <sbhr574@gmail.com>
2023-09-06 21:28:12 +0530

    RemoveDuplicates updated

--- src/Practice/IBM/RemoveDuplicates.java
+++ src/Practice/IBM/RemoveDuplicates.java
@@ -19,4 +19,9 @@ public class RemoveDuplicates
 
         return leftPointer;
     }
+
+    public static void main(String[] args) {
+        int nums[] = {0,0,1,1,1,2,2,3,3,4};
+        System.out.println(removeDuplicates(nums));
+    }
 }
