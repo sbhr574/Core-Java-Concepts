01062f239fc6cafbafbd7b3d67de7ab89855fe8d
subhajit.roy.ic <subhajit.roy.ic@gmail.com>
2024-06-08 21:29:07 +0530

    Check prime logic added

--- src/Practice/CheckPrimeOrNot.java
+++ src/Practice/CheckPrimeOrNot.java
@@ -1,10 +1,44 @@
 package Practice;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class CheckPrimeOrNot
 {
-    public static void main(String[] args) {
+
+    /**
+     * Below logic is to check on range
+     */
+    private static void test()
+    {
+        int start = 1;
+        List<Integer> prime = new ArrayList<>();
+
+        while(start<=20)
+        {
+            boolean val = true;
+
+            if (start != 0 || start != 1) {
+                for (int i = 2; i < start; i++) // line 23 to 28 is the main logic to check the prime number. Its simple and good logic for interview.
+                {
+                    if (start % i == 0)
+                    {
+                        val = false;
+                    }
+                }
+            }
+            if (val) prime.add(start);
+            start++;
+        }
+        System.out.println(prime);
+    }
+
+    public static void main(String[] args)
+    {
+        test();
+
+        
         Random random = new Random();
         int num = random.nextInt(3);
         int count = 0;
